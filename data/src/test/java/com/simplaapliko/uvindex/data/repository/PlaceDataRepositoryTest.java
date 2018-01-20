b9dd48861e4131e9106c0989991eb094e1391973
Oleg Kan <oleg.kan@hotmail.com>
2018-01-20 13:25:46 -0500

    test circleci - test should fail

--- data/src/test/java/com/simplaapliko/uvindex/data/repository/PlaceDataRepositoryTest.java
+++ data/src/test/java/com/simplaapliko/uvindex/data/repository/PlaceDataRepositoryTest.java
@@ -105,8 +105,8 @@ public class PlaceDataRepositoryTest {
 
     @Test
     public void allPlaces_noData() {
-        List<Place> mockPlaces = new ArrayList<>();
-        List<PlaceEntity> mockPlaceEntities = new ArrayList<>();
+        List<Place> mockPlaces = null;
+        List<PlaceEntity> mockPlaceEntities = null;
 
         when(mockPlaceDataSource.select()).thenReturn(mockPlaceEntities);
         when(mockPlaceEntityMapper.toModels(mockPlaceEntities)).thenReturn(mockPlaces);
